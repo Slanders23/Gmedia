9e5bcbae0f0bed601132f417f992594ec820b39e
Stephen <stephenlandes05@gmail.com>
2026-03-15 22:44:52 +0000

    Main Menu working

--- main.java
+++ main.java
@@ -13,54 +13,74 @@ class main {
                 "___________________________________\n \n             GMEDIA\n___________________________________\n");
         List<Course> courseList = new ArrayList<>();
         boolean active = true;
+        String menu;
         Integer choice;
         Scanner myObj = new Scanner(System.in); // initialize scanner
         while (active) {
-            System.out.println("Enter 1 to add new user: ");
-            System.out.print("Enter 2 to add new Course: ");
-            choice = myObj.nextInt();
-            myObj.nextLine();
-            if (choice == 2) {
-                Course c = new Course(); // Initialize Course class
+            System.out.println(
+                    "--------------------------------------------------------------------------------------------------------------------- ");
+            System.out.println(
+                    " |Enter 'z' for courses| |Enter 'x' for FYP| |Enter 'y' for Friends| Enter 'c' for Profile| |Enter 'v' for Settings|  ");
+            System.out.println(
+                    "--------------------------------------------------------------------------------------------------------------------- ");
+            menu = myObj.next();
 
-                List<Integer> tempPar = new ArrayList<>(); // initialize temp list
+            if (menu.equals("v")) {
 
-                System.out.print("\n Enter Golf Course Name: ");
-                c.setName(myObj.nextLine());
-
-                System.out.print("\n Enter how many Holes: ");
-                c.setHoles(myObj.nextInt());
-
-                for (int i = 1; i <= c.holes; i++) {
-                    System.out.print("\n Enter par for Hole " + i + ": ");
-                    tempPar.add(myObj.nextInt());
-                }
-                c.setPars(tempPar);
+                System.out.println("Enter 1 to add new user: ");
+                System.out.println("Enter 2 to add new Course: ");
+                choice = myObj.nextInt();
                 myObj.nextLine();
-                System.out.print("\n Enter Golf Course Location: ");
-                c.setLocation(myObj.nextLine());
 
-                System.out.print("\n Enter Course Record: ");
-                c.setRecord(myObj.nextInt());
-                System.out.print("\n Enter Course Index: ");
-                c.setIndex(myObj.nextDouble());
+                if (choice == 1) {
 
-                courseList.add(c);
-            }
-            System.out.println("\n--------------------------------------------------\n");
-            for (int i = 0; i < courseList.size(); i++) {
+                }
+                // ---------------------------------------------------------
+                // Creating a new Golf Course
+                // ---------------------------------------------------------
+                if (choice == 2) {
+                    Course c = new Course(); // Initialize Course class
 
-                System.out.println(courseList.get(i).name);
-                System.out.println(courseList.get(i).pars.toString());
-                System.out.println("Par: " + courseList.get(i).par);
-                System.out.println("Holes: " + courseList.get(i).holes);
-                System.out.println("Location: " + courseList.get(i).location);
-                // System.out.println("Tees: " + courseList.get(i).tee);
-                System.out.println("Course Record: " + courseList.get(i).record);
-                System.out.println("Course Index: " + courseList.get(i).index);
-                System.out.println("\nPress " + (i + 1) + " to Start Round");
+                    List<Integer> tempPar = new ArrayList<>(); // initialize temp list
+
+                    System.out.print("\n Enter Golf Course Name: ");
+                    c.setName(myObj.nextLine());
+
+                    System.out.print("\n Enter how many Holes: ");
+                    c.setHoles(myObj.nextInt());
+
+                    for (int i = 1; i <= c.holes; i++) {
+                        System.out.print("\n Enter par for Hole " + i + ": ");
+                        tempPar.add(myObj.nextInt());
+                    }
+                    c.setPars(tempPar);
+                    myObj.nextLine();
+                    System.out.print("\n Enter Golf Course Location: ");
+                    c.setLocation(myObj.nextLine());
+
+                    System.out.print("\n Enter Course Record: ");
+                    c.setRecord(myObj.nextInt());
+                    System.out.print("\n Enter Course Index: ");
+                    c.setIndex(myObj.nextDouble());
+
+                    courseList.add(c);
+                }
+            } else if (menu.equals("z")) {
                 System.out.println("\n--------------------------------------------------\n");
+                for (int i = 0; i < courseList.size(); i++) {
 
+                    System.out.println(courseList.get(i).name);
+                    System.out.println(courseList.get(i).pars.toString());
+                    System.out.println("Par: " + courseList.get(i).par);
+                    System.out.println("Holes: " + courseList.get(i).holes);
+                    System.out.println("Location: " + courseList.get(i).location);
+                    // System.out.println("Tees: " + courseList.get(i).tee);
+                    System.out.println("Course Record: " + courseList.get(i).record);
+                    System.out.println("Course Index: " + courseList.get(i).index);
+                    System.out.println("\nPress " + (i + 1) + " to Start Round");
+                    System.out.println("\n--------------------------------------------------\n");
+
+                }
             }
         }
     }
